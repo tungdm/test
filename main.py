bb09cc4a920326f9e17b8d8c181b6b459ab931e2
tungdm <minhtung3131993@gmail.com>
2018-12-08 09:52:56 +0700

    ngoc say hello

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import sys
 
 
+def test(i):
+    return i*2
+
+
 def main(name):
-    print('Hello, ', name)
+    j = 0
+    for i in range(10):
+        j += test(i)
+
+    print('Hello, %s, %s' % (name, i))
 
 
 if __name__ == '__main__':
